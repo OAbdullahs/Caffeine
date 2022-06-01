ef8f12c22e18ca28ca3f4f92646c1b9e06d5c1bd
Abdullah Alomair <AbdullahAlomair.2020@gmail.com>
2022-06-01 17:28:34 +0300

    Added app icon

--- Caffeine/main.py
+++ Caffeine/main.py
@@ -1,4 +1,5 @@
 import customtkinter
+from PIL import Image, ImageTk
 from modules.main_screen import (time_frame, file_observer)
 
 customtkinter.set_appearance_mode("System")
@@ -15,6 +16,7 @@ x_cordinate = int((screen_width / 2) - (window_width / 2))
 y_cordinate = int((screen_height / 2) - (window_height / 2))
 
 app.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+app.wm_iconphoto(False, ImageTk.PhotoImage(Image.open("icons/caffeine.ico")))
 
 if __name__ == '__main__':
     time_frame.TimeFrame(app)
