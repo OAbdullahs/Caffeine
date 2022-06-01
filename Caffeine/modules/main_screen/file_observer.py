f4c95fd2dc50a6c65ac84b22aa5edda65cffc3c4
Abdullah Alomair <AbdullahAlomair.2020@gmail.com>
2022-06-01 21:24:48 +0300

    Added file watcher

--- Caffeine/modules/main_screen/file_observer.py
+++ Caffeine/modules/main_screen/file_observer.py
@@ -1,9 +1,16 @@
+import os
+import threading
+
 import customtkinter
 import tkinter
 from tkinter.filedialog import askopenfilename
 
+from modules.caffeine.caffeine import _enable_coffeine, _disable_coffeine
+from shared.file_watcher import MyWatchDog
+
 
 class FileObserverFrame:
+    __is_caffeine_enabled = False
 
     def __init__(self, app: customtkinter.CTk):
         customtkinter.CTkLabel(text="", master=app, height=1).grid(row=3, column=0)
@@ -20,8 +27,23 @@ class FileObserverFrame:
                                                      command=self.__on_file_picker_button_clicked)
         file_picker_button.grid(row=0, column=1, pady=10)
 
-    @staticmethod
-    def __on_file_picker_button_clicked():
+        self.watching_status_frame = customtkinter.CTkFrame(master=app)
+        self.watching_status_label = customtkinter.CTkLabel(master=self.watching_status_frame)
+        self.watching_status_label.pack(side=tkinter.TOP, expand=tkinter.YES, fill=tkinter.BOTH)
+
+    def __on_file_picker_button_clicked(self):
         tkinter.Tk().withdraw()
-        file_name = askopenfilename()
-        print("file name = " + file_name)
+        filepath = askopenfilename()
+        if filepath != "":
+            self.watching_status_frame.grid(row=5, column=1, sticky="nsew")
+            self.watching_status_label["text"] = f"Watching: \n{os.path.basename(filepath)}"
+            my_watcher = MyWatchDog(filepath)
+            my_watcher.on_finish_callback = self.__on_file_watch_complete
+            threading.Thread(target=my_watcher.run).start()
+            threading.Thread(target=_enable_coffeine).start()
+        else:
+            self.watching_status_frame.grid_forget()
+
+    def __on_file_watch_complete(self, filename):
+        threading.Thread(target=_disable_coffeine).start()
+        self.watching_status_label["text"] = f"Done Watching: \n{filename}"
---
+++ Caffeine/shared/file_watcher.py
@@ -0,0 +1,47 @@
+import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+__file_size = None
+
+
+class MyWatchDog:
+    __keep_watching = True
+
+    on_finish_callback = None
+
+    def __init__(self, filepath):
+        self.event_handler = Handler()
+        self.event_handler.on_finish_callback = self.on_finish
+        self.observer = Observer()
+        self.filepath = filepath
+
+    def run(self):
+        print("Watching " + self.filepath)
+        self.event_handler.filepath = self.filepath
+        self.observer.schedule(self.event_handler, os.path.dirname(self.filepath), recursive=True)
+        self.observer.start()
+        try:
+            while self.__keep_watching:
+                time.sleep(1)
+        except:
+            self.observer.stop()
+            print("Observer Stopped")
+
+        self.observer.join()
+
+    def on_finish(self):
+        print("Stopped watching " + self.filepath)
+        self.__keep_watching = False
+        self.observer.stop()
+        self.on_finish_callback(os.path.basename(self.filepath))
+
+
+class Handler(FileSystemEventHandler):
+    filepath = None
+    on_finish_callback = None
+
+    def on_any_event(self, event):
+        if event.src_path == self.filepath:
+            self.on_finish_callback()
